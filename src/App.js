221f82dd66b17f6cc9965c53f2b1253540b2ad1d
scliff6262 <sclifford6262@gmail.com>
2019-03-30 15:03:44 -0400

    added some basic structure to about section - added MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './views/Home'
+import { Face, Group } from '@material-ui/icons'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Router>
-          <Switch>
-            <Route path='/home' component={Home}/>
-          </Switch>
-        </Router>
+      <div id="app-container">
+        <div id="about-container">
+          <div id="icon-container">
+            <h2 className="section-title">ABOUT</h2>
+            <div className="trait-container">
+              <Face/>
+            <p><b>Code</b></p>
+            </div>
+            <div className="trait-container">
+              <Group/>
+              <p><b>Teamwork</b></p>
+            </div>
+            <div className="trait-container">Well-Rounded</div>
+            <div className="trait-container">Fun</div>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 export default App;
+
+// <div>
+//   <Router>
+//     <Switch>
+//       <Route path='/' component={Home}/>
+//     </Switch>
+//   </Router>
+// </div>
